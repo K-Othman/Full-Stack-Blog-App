ac373b528ca07260e8afc51d940169b93e2fb0b0
K-Othman <karim_othman88@hotmail.com>
2024-03-16 16:19:46 +0000

    added the all the routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Single from "./pages/Single";
+import Write from "./pages/Write";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/single" element={<Single />} />
+          <Route path="/write" element={<Write />} />
         </Routes>
       </BrowserRouter>
     </div>
