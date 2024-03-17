bed9d6a823eb6a5e75ce3eb1276054100fb45d3d
K-Othman <karim_othman88@hotmail.com>
2024-03-17 14:53:49 +0000

    styling the login page

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,43 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Single from "./pages/Single";
 import Write from "./pages/Write";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import "./style.scss";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
+  );
+};
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/single" element={<Single />} />
-          <Route path="/write" element={<Write />} />
-        </Routes>
-      </BrowserRouter>
+    <div className="app">
+      <div className="contaier">
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/*"
+              element={
+                <Layout>
+                  <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/post/:id" element={<Single />} />
+                    <Route path="/write" element={<Write />} />
+                  </Routes>
+                </Layout>
+              }
+            />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
