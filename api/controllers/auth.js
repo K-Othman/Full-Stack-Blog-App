c43b065950232811c718752177fc1b37a8b28a5b
K-Othman <karim_othman88@hotmail.com>
2024-03-25 15:45:37 +0000

    created the resgister page backend and front end

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,4 +1,6 @@
 import { db } from "../db.js";
+import bcrypt from "bcrypt";
+
 export const register = (req, res) => {
   // Check Existing User
   const q = "SELECT * FROM users WHERE email = ? OR username = ?";
@@ -8,8 +10,16 @@ export const register = (req, res) => {
     if (data.length) return res.status(409).json("User already esists!");
 
     // Hash the password and create a user
-    const salt = bcrypt.genSaltSync(saltRounds);
+    const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(req.body.password, salt);
+
+    const q = "INSERT INTO users(`username`, `email`, `password`) VALUES (?)";
+    const values = [req.body.username, req.body.email, hash];
+
+    db.query(q, [values], (err, data) => {
+      if (err) return res.json(err);
+      return res.status(200).json("User has been created!");
+    });
   });
 };
 export const login = (req, res) => {};
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,16 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Resgister = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const [err, setError] = useState(null);
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("auth/register", inputs);
+      console.log(res);
+    } catch (err) {
+      setError(err.response.data);
+    }
+  };
+
   return (
     <div className="auth">
       <h1>Resgister</h1>
       <form action="" className="">
-        <input required type="text" placeholder="username" />
-        <input required type="email" placeholder="email" />
-        <input required type="password" placeholder="password" />
-        <button>Resgister</button>
-        <p>This is an error!</p>
+        <input
+          required
+          type="text"
+          placeholder="username"
+          name="username"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="email"
+          placeholder="email"
+          name="email"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="password"
+          placeholder="password"
+          name="password"
+          onChange={handleChange}
+        />
+        <button onClick={handleSubmit}> Resgister</button>
+        {err && <p>{err}</p>}
         <span>
           Do you have an account? <Link to="/login">Login</Link>{" "}
         </span>
