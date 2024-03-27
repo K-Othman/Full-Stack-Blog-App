ebe92a78729d18b12430751498bd2a7e1c6a2af6
K-Othman <karim_othman88@hotmail.com>
2024-03-27 15:30:38 +0000

    changed the dummy data to the data from the db

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,5 +1,15 @@
+import { db } from "../db.js";
+
 export const getPosts = (req, res) => {
-  res.json("It is working from Contrllers");
+  const q = req.query.cat
+    ? "SELEC * FROM post WHERE cat=?"
+    : "SELECT * FROM post";
+
+  db.query(q, [req.query.cat], (err, data) => {
+    if (err) return res.send(err);
+
+    return res.status(200).json(data);
+  });
 };
 export const getPost = (req, res) => {
   res.json("It is working from Contrllers");
