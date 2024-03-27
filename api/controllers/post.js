615007a5d90a2a3c47a7e783acc3631c52a5e081
K-Othman <karim_othman88@hotmail.com>
2024-03-27 15:19:30 +0000

    created the posts' routes

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,3 +1,15 @@
+export const getPosts = (req, res) => {
+  res.json("It is working from Contrllers");
+};
+export const getPost = (req, res) => {
+  res.json("It is working from Contrllers");
+};
 export const addPost = (req, res) => {
+  res.json("It is working from Contrllers");
+};
+export const deletePost = (req, res) => {
+  res.json("It is working from Contrllers");
+};
+export const updatePost = (req, res) => {
   res.json("It is working from Contrllers");
 };
