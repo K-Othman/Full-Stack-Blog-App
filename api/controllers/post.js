7c7d498788e1d29f6be92e1b4f3fadbf1249c27a
K-Othman <karim_othman88@hotmail.com>
2024-03-28 16:29:06 +0000

    the single page is working now

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -7,13 +7,20 @@ export const getPosts = (req, res) => {
 
   db.query(q, [req.query.cat], (err, data) => {
     if (err) return res.send(err);
-
     return res.status(200).json(data);
   });
 };
+
 export const getPost = (req, res) => {
-  res.json("It is working from Contrllers");
+  const q =
+    "SELECT `username`, `title`, `desc`, p.img, `cat`, `date` FROM users u JOIN posts p ON u.id=p.uid WHERE p.id = ?";
+  db.query(q, [req.params.id], (err, data) => {
+    if (err) return res.json(err);
+
+    return res.status(200).json(data[0]);
+  });
 };
+
 export const addPost = (req, res) => {
   res.json("It is working from Contrllers");
 };
